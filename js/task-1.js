63f38dab6046474f88ee3cd170be8dd9050ca78f
PashaKutuzov <pasha260605@gmail.com>
2024-11-13 13:56:11 +0200

    add index file

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,92 @@
 
-const log = console.log;
-function makeTransaction(quantity, pricePerDroid) {
-    //  загальна вартість замовлення
-    const totalPrice = quantity * pricePerDroid;
-    
-    //  рядок з повідомленням
-    const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    
-    //  повідомлення
-    return message;
+function isEnoughCapacity(products, containerSize){
+    let total = 0
+    const values = Object.values(products);
+    for (key of values) {
+        total += key
+      }
+      return total <= containerSize
 }
-console.log(makeTransaction(5, 3000)); 
-// "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); 
-// "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500));
- // "You ordered 10 droids worth 5000 credits!"
+
+
+
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  ); // false
+
+
+ 
+
+function calcAverageCalories(days){
+   if(days.length === 0){
+    return 0
+   }
+    
+   let totalCalories = 0
+    for (const calrs of days) {
+       totalCalories += calrs.calories 
+             
+       
+       
+     }   
+       
+    return totalCalories / days.length
+
+    }
+
+console.log(calcAverageCalories([
+      { day: "monday", calories: 3010 },
+      { day: "tuesday", calories: 3200 },
+      { day: "wednesday", calories: 3120 },
+      { day: "thursday", calories: 2900 },
+      { day: "friday", calories: 3450 },
+      { day: "saturday", calories: 3280 },
+      { day: "sunday", calories: 3300 }
+    ]) );
+ 
+// 3180
+
+console.log(
+    calcAverageCalories([])
+  );
+
+
+
+const profile = {
+    username: "Jacob",
+  playTime: 300,
+  changeUsername(newName){
+this.username = newName
+  },
+  updatePlayTime(hours){
+this.playTime += hours
+  },
+  getInfo(){
+return `${this.username} has ${this.playTime} active hours!`
+  }
+};
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
+
+
